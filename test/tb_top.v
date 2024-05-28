b0b173fca972cff4adb3726cd8aef538d12ced07
Emery Nagy <enagyjr@nagy-fam.com>
2024-05-27 20:54:53 -0700

    fix error

--- test/tb_top.v
+++ test/tb_top.v
@@ -6,7 +6,7 @@ module tb_top ();
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
-    $dumpvars(0, tb);
+    $dumpvars(0, tb_top);
     #1;
   end
 
