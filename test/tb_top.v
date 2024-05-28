5a225cad1862e17c7503b620c95af9b6b2c1bf32
Emery Nagy <enagyjr@nagy-fam.com>
2024-05-27 22:16:19 -0700

    re-add some asserts

--- test/tb_top.v
+++ test/tb_top.v
@@ -5,7 +5,7 @@ module tb_top ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
-    $dumpfile("tb.vcd");
+    $dumpfile("tb_top.vcd");
     $dumpvars(0, tb_top);
     #1;
   end
